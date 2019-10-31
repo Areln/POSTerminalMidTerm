2566c9c05ecb6ca93143b33e16abb03b53b2782f
Aaron <devilfruits1999@gmail.com>
2019-10-31 13:01:18 -0400

    Added Item class

---
+++ POSTerminalMidTerm/Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POSTerminalMidTerm
+{
+    public enum ItemType { Food, Entertainment, Clothes, Household}
+
+    class Item
+    {
+        public string Name { get; set; }
+        public ItemType Type { get; set; }
+        public double Price { get; set; }
+        public double TaxRate { get; set; }
+
+        public Item() { }
+        public Item(string name, ItemType type, double price) 
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Type}, {Price:C2}";
+        }
+    }
+}
